ba9afc290b649b36beac88baa244adeca2c3004c
Felipe <fpizarro.ux@gmail.com>
2022-02-09 18:05:54 +0100

    someScrolling

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,21 @@ import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
 AOS.init();
 
+$('a[href^="#"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+
+    });
+
+
+
 // You can also pass an optional settings object
 // below listed default settings
 
